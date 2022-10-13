ba694f9469ef740e03ec6fdff9e4812d3b807d80
Dmytro <ihnatenko.dev@gmail.com>
2022-10-13 13:41:52 +0300

    Impl DI, use Container from inversify

--- src/main.ts
+++ src/main.ts
@@ -1,16 +1,21 @@
+import { Container } from 'inversify';
 import { App } from './app';
 import { ExceptionFilter } from './errors/exception.filter';
+import { IExceptionFilter } from './errors/exception.filter.interface';
+import { ILogger } from './logger/logger.interface';
 import { LoggerService } from './logger/logger.service';
+import { TYPES } from './types';
 import { UserController } from './users/user.controller';
 
-const bootstrap = async () => {
-	const logger = new LoggerService();
-	const app = new App(
-		logger,
-		new UserController(logger),
-		new ExceptionFilter(logger)
-	);
-	await app.init();
-};
+const appContainer = new Container();
 
-bootstrap();
+appContainer.bind<ILogger>(TYPES.ILogger).to(LoggerService);
+appContainer.bind<IExceptionFilter>(TYPES.ExceptionFilter).to(ExceptionFilter);
+appContainer.bind<UserController>(TYPES.UserController).to(UserController);
+appContainer.bind<App>(TYPES.Application).to(App);
+
+const app = appContainer.get<App>(TYPES.Application);
+
+app.init();
+
+export { app, appContainer };
